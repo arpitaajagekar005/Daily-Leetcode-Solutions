93c7e800fb38ed3e36c1aa3eedeebf6fe206886f
arpitaajagekar005 <arpitaajagekar.ds@gmail.com>
2026-03-06 22:10:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -1,24 +1,10 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        int [] ans1 = new int[n];
-        int [] ans2 = new int[n];
         int [] res = new int [2*n];
 
         for(int i=0; i<n; i++){
-            ans1[i] = nums[i];
-            ans2[i] = nums[n+i];
-        }
-        int k=0;
-        int l=0;
-        for(int i=0; i<2*n; i++){
-            if((i+1)%2 != 0){
-                res[i] = ans1[k];
-                k++;
-            }
-            else{
-                res[i] = ans2[l];
-                l++;
-            }
+           res[2*i] = nums[i];
+           res[2*i+1] = nums[n+i];
         }
         return res;
     }
