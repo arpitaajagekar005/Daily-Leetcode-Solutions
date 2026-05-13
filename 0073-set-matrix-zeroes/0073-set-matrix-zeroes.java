868e59e15fc0703a00ea65371fff4a9c3a77d45d
arpitaajagekar005 <arpitaajagekar.ds@gmail.com>
2026-05-13 21:15:20 +0530

    Time: 3 ms (20.77%), Space: 47.5 MB (79.78%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -14,7 +14,6 @@ class Solution {
 
         for(int i=0; i < matrix.length; i++){
             for(int j=0; j < matrix[0].length; j++){
-                int k = 0;
                 if( row.contains(i) || col.contains(j)){
                     matrix[i][j] = 0;
                 }
