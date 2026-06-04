21b9ba4dae31c20eed280c75ce223e8ab64acfb2
arpitaajagekar005 <arpitaajagekar.ds@gmail.com>
2026-06-04 16:40:42 +0530

    Time: 4 ms (14.91%), Space: 47.5 MB (78.86%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,5 +1,4 @@
 class Solution {
-    private static final int INFINITY_MARKER = -999999;
     public void setZeroes(int[][] matrix) {
         HashSet<Integer> rows = new HashSet<>();
         HashSet<Integer> cols = new HashSet<>();
@@ -7,36 +6,18 @@ class Solution {
         for(int i = 0; i < matrix.length; i++){
             for(int j = 0; j < matrix[0].length; j++){
                 if(matrix[i][j] == 0){
-                    set_infinity(matrix, i, j);
+                    rows.add(i);
+                    cols.add(j);
                 }
             }
         }
 
         for(int i = 0; i < matrix.length; i++){
             for(int j = 0; j < matrix[0].length; j++){
-                if(matrix[i][j] == INFINITY_MARKER){
+                if(rows.contains(i) || cols.contains(j)){
                     matrix[i][j] = 0;
                 }
             }
         }
-             
-
-        
-    }
-    void set_infinity(int [][]matrix, int rows, int cols){
-        int r = matrix.length;
-        int c = matrix[0].length;
-
-        for(int i = 0; i < r; i++){
-            if(matrix[i][cols] != 0){
-                matrix[i][cols] = INFINITY_MARKER;
-            }
-        }
-        for(int i = 0; i < c; i++){
-            if(matrix[rows][i] != 0){
-                matrix[rows][i] = INFINITY_MARKER;
-            }
-        }
-        
     }
 }
