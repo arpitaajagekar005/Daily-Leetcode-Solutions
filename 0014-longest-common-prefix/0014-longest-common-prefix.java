bcac3568bf818369bcf8a9e8a52d95b08246ce35
arpitaajagekar005 <arpitaajagekar.ds@gmail.com>
2026-07-06 15:38:10 +0530

    Time: 5 ms (5.34%), Space: 43.6 MB (5.2%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,6 +1,6 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        StringBuilder res = new StringBuilder();
+        String res ="";
         if(strs.length == 0){
             return "";
         }
@@ -8,11 +8,11 @@ class Solution {
         for(int i = 0; i < strs[0].length(); i++){
             for(int j = 1; j < strs.length; j++){
                 if( i == strs[j].length() || strs[0].charAt(i) != strs[j].charAt(i)){
-                    return res.toString();
+                    return res;
                 } 
             }
-            res.append(strs[0].charAt(i));  
+            res += strs[0].charAt(i);  
         }
-        return res.toString();
+        return res;
     }
 }
